4e3b73fec88747f18ca751ff5076c394f289834c
Tamas Gal <himself@tamasgal.com>
2025-04-03 22:17:46 +0200

    Restructure

--- resqui/src/resqui/cli.py
+++ resqui/src/resqui/cli.py
@@ -1,2 +1,24 @@
+"""
+Usage:
+    resqui -c <config_file>
+    resqui [--version] [--help]
+
+Options:
+    -c <config_file>  Path to the configuration file.
+    --version         Show the version of the script.
+    --help            Show this help message."
+"""
+from .docopt import docopt
+from .version import __version__
+
+
+class Configuration:
+    pass
+
+
 def resqui():
-    print("joP")
+    args = docopt(__doc__, version=__version__)
+    print(args)
+
+    # configuration = Configuration()
+    # pipeline = configuration.get_pipeline(args['-c'])
