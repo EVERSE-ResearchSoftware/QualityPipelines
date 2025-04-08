8ecc8dbfd389a34894980b110cb4d2b0bec638e6
Tamas Gal <himself@tamasgal.com>
2025-04-08 11:32:48 +0200

    Add a spinner to indicate progress

--- resqui/src/resqui/cli.py
+++ resqui/src/resqui/cli.py
@@ -8,8 +8,12 @@ Options:
     --version         Show the version of the script.
     --help            Show this help message.
 """
+import itertools
+import time
+import threading
 import importlib
 import json
+import sys
 from .docopt import docopt
 from .version import __version__
 
@@ -20,6 +24,43 @@ class Configuration:
             self._cfg = json.load(f)
 
 
+class Spinner:
+    """
+    A simple spinner class to indicate progress in the console.
+    Use it as a context manager.
+    """
+
+    def __init__(self):
+        self.spinning = False
+        self.spinner_thread = None
+
+    def start(self):
+        self.spinning = True
+        self.spinner_thread = threading.Thread(target=self._spinner)
+        self.spinner_thread.start()
+
+    def stop(self):
+        self.spinning = False
+        if self.spinner_thread:
+            self.spinner_thread.join()
+
+    def _spinner(self):
+        for char in itertools.cycle("|/-\\"):
+            sys.stdout.write(char)
+            sys.stdout.flush()
+            time.sleep(0.1)
+            sys.stdout.write("\b")
+            if not self.spinning:
+                break
+
+    def __enter__(self):
+        self.start()
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.stop()
+
+
 def resqui():
     args = docopt(__doc__, version=__version__)
 
@@ -27,10 +68,21 @@ def resqui():
     url = args["<repository_url>"]
 
     for indicator in configuration._cfg["indicators"]:
-        base_package = __name__.rsplit(".", 1)[0]
-        plugin_class = indicator["plugin"]
-        plugin_module = importlib.import_module(base_package + ".plugins")
-        plugin_instance = getattr(plugin_module, plugin_class)()
-        plugin_method = indicator["name"]
-        result = getattr(plugin_instance, plugin_method)(url)
-        print(f"  {indicator['name']}: {result}")
+        print(
+            f"  Checking indicator {indicator['name']}/{indicator['plugin']}:",
+            end=" ",
+        )
+        sys.stdout.flush()
+
+        with Spinner():
+            base_package = __name__.rsplit(".", 1)[0]
+            plugin_class = indicator["plugin"]
+            plugin_module = importlib.import_module(base_package + ".plugins")
+            plugin_instance = getattr(plugin_module, plugin_class)()
+            plugin_method = indicator["name"]
+            result = getattr(plugin_instance, plugin_method)(url)
+
+        if result:
+            print("\033[92mTrue\033[0m")
+        else:
+            print("\033[91mFalse\033[0m")
