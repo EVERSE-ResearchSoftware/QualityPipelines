f127677ef81cb29575cae64eae974f2cb06f46d3
Tamas Gal <himself@tamasgal.com>
2025-04-08 11:17:36 +0200

    Prototype implementation

--- resqui/src/resqui/cli.py
+++ resqui/src/resqui/cli.py
@@ -1,24 +1,36 @@
 """
 Usage:
-    resqui -c <config_file>
-    resqui [--version] [--help]
+    resqui -c <config_file> <repository_url>
 
 Options:
+    <repository_url>  URL of the repository to be analyzed.
     -c <config_file>  Path to the configuration file.
     --version         Show the version of the script.
-    --help            Show this help message."
+    --help            Show this help message.
 """
+import importlib
+import json
 from .docopt import docopt
 from .version import __version__
 
 
 class Configuration:
-    pass
+    def __init__(self, filepath):
+        with open(filepath) as f:
+            self._cfg = json.load(f)
 
 
 def resqui():
     args = docopt(__doc__, version=__version__)
-    print(args)
 
-    # configuration = Configuration()
-    # pipeline = configuration.get_pipeline(args['-c'])
+    configuration = Configuration(args["-c"])
+    url = args["<repository_url>"]
+
+    for indicator in configuration._cfg["indicators"]:
+        base_package = __name__.rsplit(".", 1)[0]
+        plugin_class = indicator["plugin"]
+        plugin_module = importlib.import_module(base_package + ".plugins")
+        plugin_instance = getattr(plugin_module, plugin_class)()
+        plugin_method = indicator["name"]
+        result = getattr(plugin_instance, plugin_method)(url)
+        print(f"  {indicator['name']}: {result}")
---
+++ resqui/src/resqui/plugins.py
@@ -0,0 +1,166 @@
+import os
+import venv
+import subprocess
+import tempfile
+import json
+
+
+def normalized(script):
+    """
+    Removes extra indentation from a script, caused by triple quotes.
+    This does not support mixed tab/spaces.
+    """
+    lines = script.splitlines()
+    lines = [line for line in lines if line.strip()]  # remove empty lines
+    first_line = lines[0]
+    leading_whitespace = len(first_line) - len(first_line.lstrip())
+    return "\n".join(line[leading_whitespace:] for line in lines)
+
+
+class HowFairIs:
+    indicators = ["has_license"]
+    version = "0.14.2"
+
+    def __init__(self):
+        self.instantiate()
+
+    def instantiate(self):
+        self.temp_dir = tempfile.mkdtemp()
+        venv.create(self.temp_dir, with_pip=True)
+        try:
+            subprocess.run(
+                [
+                    f"{self.temp_dir}/bin/pip",
+                    "install",
+                    f"howfairis=={self.version}",
+                ],
+                check=True,
+                stdout=subprocess.DEVNULL,
+                stderr=subprocess.DEVNULL,
+            )
+        except subprocess.CalledProcessError as e:
+            print(f"Error installing howfairis: {e}")
+            raise
+
+    def has_license(self, url):
+        script = normalized(
+            f"""
+        from howfairis import Repo, Checker
+        url = "{url}"
+        repo = Repo(url)
+        checker = Checker(repo, is_quiet=True)
+        print(checker.has_license())
+        """
+        )
+        result = self.execute(script)
+        return result.stdout.strip() == "True"
+
+    def execute(self, script):
+        return subprocess.run(
+            [f"{self.temp_dir}/bin/python", "-c", script],
+            capture_output=True,
+            text=True,
+        )
+
+
+class CFFConvert:
+    indicators = ["has_citation"]
+    version = "2.0.0"
+
+    def __init__(self):
+        self.instantiate()
+
+    def instantiate(self):
+        self.temp_dir = tempfile.mkdtemp()
+        venv.create(self.temp_dir, with_pip=True)
+        try:
+            subprocess.run(
+                [
+                    f"{self.temp_dir}/bin/pip",
+                    "install",
+                    f"cffconvert=={self.version}",
+                ],
+                check=True,
+                stdout=subprocess.DEVNULL,
+                stderr=subprocess.DEVNULL,
+            )
+        except subprocess.CalledProcessError as e:
+            print(f"Error installing cffconvert: {e}")
+            raise
+
+    def has_citation(self, url):
+        script = normalized(
+            f"""
+            from cffconvert.cli.create_citation import create_citation
+            citation = create_citation(None, "{url}")
+            if citation.validate() is None:
+                print("True")
+        """
+        )
+        result = self.execute(script)
+        return result.stdout.strip() == "True"
+
+    def execute(self, script):
+        return subprocess.run(
+            [f"{self.temp_dir}/bin/python", "-c", script],
+            capture_output=True,
+            text=True,
+        )
+
+
+class Gitleaks:
+    indicators = ["has_security_leak"]
+    version = "8.24.2"
+
+    def __init__(self):
+        self.instantiate()
+
+    def instantiate(self):
+        try:
+            subprocess.run(
+                [
+                    "docker",
+                    "pull",
+                    f"ghcr.io/gitleaks/gitleaks:v{self.version}",
+                ],
+                check=True,
+                stdout=subprocess.DEVNULL,
+                stderr=subprocess.DEVNULL,
+            )
+        except subprocess.CalledProcessError as e:
+            print(f"Error pulling the Gitleaks Docker image: {e}")
+            raise
+
+    def has_security_leak(self, url):
+        temp_dir = tempfile.mkdtemp()
+        report_fname = "report.json"
+
+        try:
+            subprocess.run(
+                ["git", "clone", url, temp_dir],
+                check=True,
+                stdout=subprocess.DEVNULL,
+                stderr=subprocess.DEVNULL,
+            )
+        except subprocess.CalledProcessError as e:
+            print(f"Error cloning {url}: {e}")
+            raise
+
+        cmd = [
+            "docker",
+            "run",
+            "-v",
+            f"{temp_dir}:/path",
+            f"ghcr.io/gitleaks/gitleaks:v{self.version}",
+            "git",
+            "/path",
+            "-r",
+            f"/path/{report_fname}",
+        ]
+        subprocess.run(cmd, capture_output=True, text=True)
+        with open(os.path.join(temp_dir, report_fname)) as f:
+            report = json.load(f)
+
+        if report:
+            return True
+        return False
