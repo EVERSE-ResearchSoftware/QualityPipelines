1feecd9664f906e947de4a9ab4e12467bb9db38d
Tamas Gal <himself@tamasgal.com>
2025-07-12 10:12:45 +0200

    Change schema

--- src/resqui/core.py
+++ src/resqui/core.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from datetime import datetime
 from dataclasses import dataclass
 from typing import Optional
 import json
@@ -21,27 +22,57 @@ class Configuration:
             self._cfg = json.load(f)
 
 
+@dataclass
+class CheckResult:
+    """
+    Datatype for indicator check results.
+    """
+
+    process: str = "Undefined process"
+    status_id: str = "missing"
+    output: str = "missing"
+    evidence: str = "missing"
+    success: bool = False
+
+    def __bool__(self):
+        return self.success
+
+
 class Summary:
     """
-    Summary of the results of the checks.
+    Summary of the software quality assessment.
     """
 
     def __init__(self):
         self.checks = []
 
-    def add_indicator_result(self, indicator, checking_software, status):
+    def add_indicator_result(self, indicator, checking_software, result):
         self.checks.append(
             {
-                "checking_software": {
+                "@type": "CheckResult",
+                "assessesIndicator": {"@id": indicator["@id"]},
+                "checkingSoftware": {
+                    "@type": "schema:SoftwareApplication",
                     "name": checking_software.name,
-                    "id": checking_software.id,
-                    "version": checking_software.version,
+                    "@id": checking_software.id,
+                    "softwareVersion": checking_software.version,
                 },
-                "indicator": indicator,
-                "status": status,
+                "process": result.process,
+                "status": {"@id": result.status_id},
+                "output": result.output,
+                "evidence": result.evidence,
             }
         )
 
     def to_json(self, filename):
         with open(filename, "w") as f:
-            json.dump(self.checks, f, indent=4)
+            json.dump(
+                {
+                    "@context": "https://w3id.org/everse/rsqa/0.0.1/",
+                    "@type": "SoftwareQualityAssessment",
+                    "dateCreated": str(datetime.now()),
+                    "checks": self.checks,
+                },
+                f,
+                indent=4,
+            )
