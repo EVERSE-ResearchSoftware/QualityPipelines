4292fed23b9a282860f349feaee931bb051c1f41
Tamas Gal <himself@tamasgal.com>
2025-07-21 11:44:50 +0200

    Improve docstring

--- src/resqui/plugins/__init__.py
+++ src/resqui/plugins/__init__.py
@@ -13,6 +13,10 @@ def normalized(script):
     """
     Removes extra indentation from a script, caused by triple quotes.
     This does not support mixed tab/spaces.
+
+    Use it for better readability in Python sources, where e.g. a shell
+    script or Python script in triple quotes needs to be created and
+    passed to a subprocess.
     """
     lines = script.splitlines()
     lines = [line for line in lines if line.strip()]  # remove empty lines
